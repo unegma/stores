e73017c85ed13ee07de0dd0775f9f30f9188243d
Timothy Coleman <timhc22@gmail.com>
2022-11-07 13:02:05 +0000

    Move hamburger button to top

--- src/App.tsx
+++ src/App.tsx
@@ -134,10 +134,6 @@ function App() {
         />
       </Routes>
 
-      <div className={`buttons-container buttons-container--left`}>
-        <Menu className="pointer" style={{ color: "black", margin: "0 4px" }} onClick={(event) => {toggleLeftSideDrawer(event)}}/>
-      </div>
-
       <div className="buttons-container">
         {/*<InfoOutlined className="pointer" style={{ color: "black", margin: "0 4px" }} onClick={() => {setShowInfoModal(!showInfoModal)}}/>*/}
 
